9734d25e07b71e77e607d777462175143638a024
Enrique Zapata S <enrique.murdock@gmail.com>
2024-07-17 18:28:26 +0200

    Se crean funciones para cada parte del programa Se han añadido la lógica para que identifique el sistema operativo y sepa como tratar los archivos de entrada y salida Se agregan mensajes generales del avance de la ejecución

--- mxntoeur.py
+++ mxntoeur.py
@@ -2,23 +2,65 @@ import os
 import sys
 
 
-args = sys.argv[1:]
 
-if ( len(sys.argv) != 2 ):
-    print("Necesitas colocar el nombre del archivo de entrada.")
-    exit(1)
 
-if ( not os.path.isfile(sys.argv[1]) ):
-    print("El archivo no es valido.")
-    exit(1)
+def validate_params(args):
+    if ( len(args) != 2 ):
+        print("Necesitas colocar el nombre del archivo de entrada.")
+        print("Ejemplo: " + args[0] + " archivo.txt")
+        exit(1)
+    return(0)
 
-same_path, same_file = os.path.split(sys.argv[1])
 
-f = open(sys.argv[1], "r")
-nf = open(same_path + "/new_" + same_file, "w")
+def validate_file(args):
+    if ( not os.path.isfile(args[1]) ):
+        print("El archivo no puede leerse.")
+        if ( os.name == "nt" ):
+            print("Intenta colocar el nombre entre comillas")
+            print("Ejemplo: " + args[0] + " \"C:\archivo.txt\"\n")
+            print("o colocar el caracter \ de la siguiente forma \\")
+            print("Ejemplo: " + args[0] + " C:\\\\archivo.txt")
+        exit(1)
 
-for line in f:
-    fields = line.split(",", 2)
-    nf.write(fields[0] + ";" + fields[1] + ";" + fields[2].replace("\n","").replace("\r", "").replace(",", "").replace(".", ",") + "\n")
 
-exit(0)
+def open_files(args):
+    same_path, same_file = os.path.split(args[1])
+
+    if ( os.name == "posix" ):
+        new_file = same_path + "/new_" + same_file
+    else:
+        new_file = same_path + "\\new_" + same_file
+    print(f"Se creara el archivo: {new_file}")
+
+    f = open(args[1], "r")
+    nf = open(new_file, "w")
+
+    return f, nf
+
+
+def write_file(f, nf):
+    for line in f:
+        fields = line.split(",", 2)
+        nf.write(fields[0] + ";" + fields[1] + ";" + fields[2].replace("\n","").replace("\r", "").replace(",", "").replace(".", ",") + "\n")
+    print("Se ha escrito el archivo correctamente.")
+
+
+def close_file(f, nf):
+    f.close()
+    nf.close()
+    print("Se han cerrado los archivos.")
+
+
+def convert_file(args):
+    validate_params(args)
+    validate_file(args)
+    f, nf = open_files(args)
+    write_file(f, nf)
+    close_file(f, nf)
+    print("Ha concluido el proceso de transformación.")
+
+
+if __name__ == '__main__':
+    args = sys.argv[0:]
+    convert_file(args)
+    exit(0)
